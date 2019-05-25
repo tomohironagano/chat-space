05ad19ad7d58b144a26e53dc902f470cffff2876
tomohironagano <t0mo.0724@icloud.com>
2019-05-25 19:10:38 +0900

    resetメソッド

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -35,7 +35,7 @@ $(document).on('turbolinks:load', function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.form__message').val('');
+      $('#new_message')[0].reset();
       scrollBottom();
       function scrollBottom(){
         var target = $('.message').last();
