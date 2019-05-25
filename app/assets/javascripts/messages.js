633eb8fc131e448b67c84c467d8c3a91a0c79332
tomohironagano <t0mo.0724@icloud.com>
2019-05-25 15:29:41 +0900

    非同期通信

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,55 @@
+$(document).on('turbolinks:load', function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <p class="upper-message__user-name">
+                      ${message.user_name}
+                    </p>
+                    <p class="upper-message__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="lower-meesage">
+                    <div>
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+  return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var message = new FormData(this);
+    var url = (window.location.href);
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+      scrollBottom();
+      function scrollBottom(){
+        var target = $('.message').last();
+        var position = target.offset().top + $('.messages').scrollTop();
+        $('.messages').animate({
+          scrollTop: position
+        }, 300, 'swing');
+      }
+    })
+    .fail(function(data){
+      alert('自動更新に失敗しました');
+    })
+    .always(function(data){
+      $('.form__submit').prop('disabled', false);
+    })
+  })
+});
